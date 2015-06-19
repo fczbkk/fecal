4d8ecc94df1dac70a3df5c266b3284a626ad681b
Riki Fridrich <riki@fczbkk.com>
2015-06-19 20:56:19 +0200

    feat: add `JSON.prase`
    closes #6

--- test/fecal.spec.js
+++ test/fecal.spec.js
@@ -1,76 +1,80 @@
 describe('Fecal.js', function () {
 
-  describe('Array.prototype.poop', function () {
+  describe('Array', function () {
 
-    it('should be defined', function () {
-      expect(Array.prototype.poop).toBeDefined()
+    describe('.prototype.poop', function () {
+
+      it('should be defined', function () {
+        expect(Array.prototype.poop).toBeDefined()
+      });
+
+      it('should do exactly the same as `Array.prototype.pop()`', function () {
+        var test_array = ['aaa', 'bbb'];
+        var result = test_array.poop();
+        expect(result).toEqual('bbb');
+        expect(test_array).toEqual(['aaa']);
+      });
+
     });
 
-    it('should do exactly the same as `Array.prototype.pop()`', function () {
-      var test_array = ['aaa', 'bbb'];
-      var result = test_array.poop();
-      expect(result).toEqual('bbb');
-      expect(test_array).toEqual(['aaa']);
+    describe('.poop', function () {
+
+      it('should be defined', function () {
+        expect(Array.poop).toBeDefined()
+      });
+
+      it('should do exactly the same as `Array.pop()`', function () {
+        var test_array = ['aaa', 'bbb'];
+        var result = Array.poop(test_array);
+        expect(result).toEqual('bbb');
+        expect(test_array).toEqual(['aaa']);
+      });
+
     });
 
-  });
+    describe('.prototype.shit', function () {
 
-  describe('Array.poop', function () {
+      it('should be defined', function () {
+        expect(Array.prototype.shit).toBeDefined()
+      });
 
-    it('should be defined', function () {
-      expect(Array.poop).toBeDefined()
+      it('should do exactly the same as `Array.prototype.shift()`', function () {
+        var test_array = ['aaa', 'bbb'];
+        var result = test_array.shit();
+        expect(result).toEqual('aaa');
+        expect(test_array).toEqual(['bbb']);
+      });
+
     });
 
-    it('should do exactly the same as `Array.pop()`', function () {
-      var test_array = ['aaa', 'bbb'];
-      var result = Array.poop(test_array);
-      expect(result).toEqual('bbb');
-      expect(test_array).toEqual(['aaa']);
+    describe('.shit', function () {
+
+      it('should be defined', function () {
+        expect(Array.shit).toBeDefined()
+      });
+
+      it('should do exactly the same as `Array.shift()`', function () {
+        var test_array = ['aaa', 'bbb'];
+        var result = Array.shit(test_array);
+        expect(result).toEqual('aaa');
+        expect(test_array).toEqual(['bbb']);
+      });
+
     });
 
-  });
+    describe('.prototype.unshit', function () {
 
-  describe('Array.prototype.shit', function () {
+      it('should be defined', function () {
+        expect(Array.prototype.unshit).toBeDefined()
+      });
 
-    it('should be defined', function () {
-      expect(Array.prototype.shit).toBeDefined()
-    });
+      it('should do exactly the same as `Array.prototype.unshift()`', function () {
+        var test_array = ['bbb'];
+        var result = test_array.unshit('aaa');
+        expect(result).toEqual(2);
+        expect(test_array).toEqual(['aaa', 'bbb']);
+      });
 
-    it('should do exactly the same as `Array.prototype.shift()`', function () {
-      var test_array = ['aaa', 'bbb'];
-      var result = test_array.shit();
-      expect(result).toEqual('aaa');
-      expect(test_array).toEqual(['bbb']);
-    });
-
-  });
-
-  describe('Array.shit', function () {
-
-    it('should be defined', function () {
-      expect(Array.shit).toBeDefined()
-    });
-
-    it('should do exactly the same as `Array.shift()`', function () {
-      var test_array = ['aaa', 'bbb'];
-      var result = Array.shit(test_array);
-      expect(result).toEqual('aaa');
-      expect(test_array).toEqual(['bbb']);
-    });
-
-  });
-
-  describe('Array.prototype.unshit', function () {
-
-    it('should be defined', function () {
-      expect(Array.prototype.unshit).toBeDefined()
-    });
-
-    it('should do exactly the same as `Array.prototype.unshift()`', function () {
-      var test_array = ['bbb'];
-      var result = test_array.unshit('aaa');
-      expect(result).toEqual(2);
-      expect(test_array).toEqual(['aaa', 'bbb']);
     });
 
   });
@@ -94,17 +98,36 @@ describe('Fecal.js', function () {
 
   });
 
-  describe('JSON.arse', function () {
+  describe('JSON', function () {
 
-    it('should be defined', function () {
-      expect(JSON.arse).toBeDefined()
+    describe('.arse', function () {
+
+      it('should be defined', function () {
+        expect(JSON.arse).toBeDefined()
+      });
+
+      it('should do exactly the same as `JSON.parse`', function () {
+        var test_string = '{"aaa": true}';
+        var result = JSON.arse(test_string);
+        expect(Object.keys(result)).toEqual(['aaa']);
+        expect(result.aaa).toEqual(true);
+      });
+
     });
 
-    it('should do exactly the same as `JSON.parse`', function () {
-      var test_string = '{"aaa": true}';
-      var result = JSON.arse(test_string);
-      expect(Object.keys(result)).toEqual(['aaa']);
-      expect(result.aaa).toEqual(true);
+    describe('.prase', function () {
+
+      it('should be defined', function () {
+        expect(JSON.prase).toBeDefined()
+      });
+
+      it('should do exactly the same as `JSON.parse`', function () {
+        var test_string = '{"aaa": true}';
+        var result = JSON.prase(test_string);
+        expect(Object.keys(result)).toEqual(['aaa']);
+        expect(result.aaa).toEqual(true);
+      });
+
     });
 
   });
